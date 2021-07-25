33a44d8c6d2e67b1dae0febad8a445c555cccfff
mustafaabubaker <mustafayabubaker@gmail.com>
2021-07-25 19:54:34 -0400

    first file

---
+++ script.js
@@ -0,0 +1,4 @@
+// i know i need an array of objects to hold my questions and answers
+// i know i'm going to need some logic moving through that array
+// i want a timer to help move the questions along as they do in the demo
+// 
